d7332cfd3ec1f2d2b98657e4b66465fd6d9e707e
DeiberV <deiberveranodev@gmail.com>
2024-02-13 19:16:25 -0500

    [UPDATE] Pokemon details

---
+++ src/pokemons/index.ts
@@ -0,0 +1,4 @@
+export * from "./interfaces/pokemos-response";
+export * from "./interfaces/simple-pokemon";
+export * from "./interfaces/pokemon";
+export { PokemonGrid } from "./components/PokemonGrid";
